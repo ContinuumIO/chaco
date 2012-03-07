af61e59ab7f8fd02be5d8b49a04dc7550c0c3dc0
Hugo <hugoshi@blob.(none)>
2012-03-07 15:15:10 -0500

    adding autogenned files so that I have backups if they get blown away

---
+++ examples/continuum/main.js
@@ -0,0 +1,134 @@
+window.export_data = {"83722608": {"index_name": "sepalLength", "data_source": 80443440, "type": "ColormappedScatterPlot", "color_name": "species", "value_name": "petalLength"}, "84374640": {"width": 65.0, "plots": {"hello": ["84489040"]}, "type": "Plot", "height": 35.0}, "85829584": {"width": 65.0, "plots": {"hello": ["85931696"]}, "type": "Plot", "height": 35.0}, "85718800": {"width": 65.0, "plots": {"hello": ["85829200"]}, "type": "Plot", "height": 35.0}, "87173360": {"index_name": "petalWidth", "data_source": 80443440, "type": "ColormappedScatterPlot", "color_name": "species", "value_name": "sepalWidth"}, "84275760": {"index_name": "sepalLength", "data_source": 80443440, "type": "ColormappedScatterPlot", "color_name": "species", "value_name": "petalWidth"}, "85718416": {"index_name": "petalLength", "data_source": 80443440, "type": "ColormappedScatterPlot", "color_name": "species", "value_name": "sepalLength"}, "82829136": {"index_name": "sepalLength", "data_source": 80443440, "type": "ColormappedScatterPlot", "color_name": "species", "value_name": "sepalLength"}, "85165648": {"width": 65.0, "plots": {"hello": ["85718416"]}, "type": "Plot", "height": 35.0}, "82919792": {"width": 65.0, "plots": {"hello": ["83615920"]}, "type": "Plot", "height": 35.0}, "80443440": {"arrays": {"sepalLength": [5.1, 4.9, 4.7, 4.6, 5.0, 5.4, 4.6, 5.0, 4.4, 4.9, 5.4, 4.8, 4.8, 4.3, 5.8, 5.7, 5.4, 5.1, 5.7, 5.1, 5.4, 5.1, 4.6, 5.1, 4.8, 5.0, 5.0, 5.2, 5.2, 4.7, 4.8, 5.4, 5.2, 5.5, 4.9, 5.0, 5.5, 4.9, 4.4, 5.1, 5.0, 4.5, 4.4, 5.0, 5.1, 4.8, 5.1, 4.6, 5.3, 5.0, 7.0, 6.4, 6.9, 5.5, 6.5, 5.7, 6.3, 4.9, 6.6, 5.2, 5.0, 5.9, 6.0, 6.1, 5.6, 6.7, 5.6, 5.8, 6.2, 5.6, 5.9, 6.1, 6.3, 6.1, 6.4, 6.6, 6.8, 6.7, 6.0, 5.7, 5.5, 5.5, 5.8, 6.0, 5.4, 6.0, 6.7, 6.3, 5.6, 5.5, 5.5, 6.1, 5.8, 5.0, 5.6, 5.7, 5.7, 6.2, 5.1, 5.7, 6.3, 5.8, 7.1, 6.3, 6.5, 7.6, 4.9, 7.3, 6.7, 7.2, 6.5, 6.4, 6.8, 5.7, 5.8, 6.4, 6.5, 7.7, 7.7, 6.0, 6.9, 5.6, 7.7, 6.3, 6.7, 7.2, 6.2, 6.1, 6.4, 7.2, 7.4, 7.9, 6.4, 6.3, 6.1, 7.7, 6.3, 6.4, 6.0, 6.9, 6.7, 6.9, 5.8, 6.8, 6.7, 6.7, 6.3, 6.5, 6.2, 5.9], "petalWidth": [0.2, 0.2, 0.2, 0.2, 0.2, 0.4, 0.3, 0.2, 0.2, 0.1, 0.2, 0.2, 0.1, 0.1, 0.2, 0.4, 0.4, 0.3, 0.3, 0.3, 0.2, 0.4, 0.2, 0.5, 0.2, 0.2, 0.4, 0.2, 0.2, 0.2, 0.2, 0.4, 0.1, 0.2, 0.2, 0.2, 0.2, 0.1, 0.2, 0.2, 0.3, 0.3, 0.2, 0.6, 0.4, 0.3, 0.2, 0.2, 0.2, 0.2, 1.4, 1.5, 1.5, 1.3, 1.5, 1.3, 1.6, 1.0, 1.3, 1.4, 1.0, 1.5, 1.0, 1.4, 1.3, 1.4, 1.5, 1.0, 1.5, 1.1, 1.8, 1.3, 1.5, 1.2, 1.3, 1.4, 1.4, 1.7, 1.5, 1.0, 1.1, 1.0, 1.2, 1.6, 1.5, 1.6, 1.5, 1.3, 1.3, 1.3, 1.2, 1.4, 1.2, 1.0, 1.3, 1.2, 1.3, 1.3, 1.1, 1.3, 2.5, 1.9, 2.1, 1.8, 2.2, 2.1, 1.7, 1.8, 1.8, 2.5, 2.0, 1.9, 2.1, 2.0, 2.4, 2.3, 1.8, 2.2, 2.3, 1.5, 2.3, 2.0, 2.0, 1.8, 2.1, 1.8, 1.8, 1.8, 2.1, 1.6, 1.9, 2.0, 2.2, 1.5, 1.4, 2.3, 2.4, 1.8, 1.8, 2.1, 2.4, 2.3, 1.9, 2.3, 2.5, 2.3, 1.9, 2.0, 2.3, 1.8], "sepalWidth": [3.5, 3.0, 3.2, 3.1, 3.6, 3.9, 3.4, 3.4, 2.9, 3.1, 3.7, 3.4, 3.0, 3.0, 4.0, 4.4, 3.9, 3.5, 3.8, 3.8, 3.4, 3.7, 3.6, 3.3, 3.4, 3.0, 3.4, 3.5, 3.4, 3.2, 3.1, 3.4, 4.1, 4.2, 3.1, 3.2, 3.5, 3.6, 3.0, 3.4, 3.5, 2.3, 3.2, 3.5, 3.8, 3.0, 3.8, 3.2, 3.7, 3.3, 3.2, 3.2, 3.1, 2.3, 2.8, 2.8, 3.3, 2.4, 2.9, 2.7, 2.0, 3.0, 2.2, 2.9, 2.9, 3.1, 3.0, 2.7, 2.2, 2.5, 3.2, 2.8, 2.5, 2.8, 2.9, 3.0, 2.8, 3.0, 2.9, 2.6, 2.4, 2.4, 2.7, 2.7, 3.0, 3.4, 3.1, 2.3, 3.0, 2.5, 2.6, 3.0, 2.6, 2.3, 2.7, 3.0, 2.9, 2.9, 2.5, 2.8, 3.3, 2.7, 3.0, 2.9, 3.0, 3.0, 2.5, 2.9, 2.5, 3.6, 3.2, 2.7, 3.0, 2.5, 2.8, 3.2, 3.0, 3.8, 2.6, 2.2, 3.2, 2.8, 2.8, 2.7, 3.3, 3.2, 2.8, 3.0, 2.8, 3.0, 2.8, 3.8, 2.8, 2.8, 2.6, 3.0, 3.4, 3.1, 3.0, 3.1, 3.1, 3.1, 2.7, 3.2, 3.3, 3.0, 2.5, 3.0, 3.4, 3.0], "petalLength": [1.4, 1.4, 1.3, 1.5, 1.4, 1.7, 1.4, 1.5, 1.4, 1.5, 1.5, 1.6, 1.4, 1.1, 1.2, 1.5, 1.3, 1.4, 1.7, 1.5, 1.7, 1.5, 1.0, 1.7, 1.9, 1.6, 1.6, 1.5, 1.4, 1.6, 1.6, 1.5, 1.5, 1.4, 1.5, 1.2, 1.3, 1.4, 1.3, 1.5, 1.3, 1.3, 1.3, 1.6, 1.9, 1.4, 1.6, 1.4, 1.5, 1.4, 4.7, 4.5, 4.9, 4.0, 4.6, 4.5, 4.7, 3.3, 4.6, 3.9, 3.5, 4.2, 4.0, 4.7, 3.6, 4.4, 4.5, 4.1, 4.5, 3.9, 4.8, 4.0, 4.9, 4.7, 4.3, 4.4, 4.8, 5.0, 4.5, 3.5, 3.8, 3.7, 3.9, 5.1, 4.5, 4.5, 4.7, 4.4, 4.1, 4.0, 4.4, 4.6, 4.0, 3.3, 4.2, 4.2, 4.2, 4.3, 3.0, 4.1, 6.0, 5.1, 5.9, 5.6, 5.8, 6.6, 4.5, 6.3, 5.8, 6.1, 5.1, 5.3, 5.5, 5.0, 5.1, 5.3, 5.5, 6.7, 6.9, 5.0, 5.7, 4.9, 6.7, 4.9, 5.7, 6.0, 4.8, 4.9, 5.6, 5.8, 6.1, 6.4, 5.6, 5.1, 5.6, 6.1, 5.6, 5.5, 4.8, 5.4, 5.6, 5.1, 5.1, 5.9, 5.7, 5.2, 5.0, 5.2, 5.4, 5.1], "species": [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2]}, "tools": "select", "type": "ArrayPlotData"}, "86493040": {"index_name": "petalLength", "data_source": 80443440, "type": "ColormappedScatterPlot", "color_name": "species", "value_name": "petalWidth"}, "83722992": {"width": 65.0, "plots": {"hello": ["84275760"]}, "type": "Plot", "height": 35.0}, "84489040": {"index_name": "sepalWidth", "data_source": 80443440, "type": "ColormappedScatterPlot", "color_name": "species", "value_name": "sepalWidth"}, "87173744": {"width": 65.0, "plots": {"hello": ["87275952"]}, "type": "Plot", "height": 35.0}, "84374256": {"index_name": "sepalWidth", "data_source": 80443440, "type": "ColormappedScatterPlot", "color_name": "species", "value_name": "sepalLength"}, "83616304": {"width": 65.0, "plots": {"hello": ["83722608"]}, "type": "Plot", "height": 35.0}, "85165264": {"index_name": "sepalWidth", "data_source": 80443440, "type": "ColormappedScatterPlot", "color_name": "species", "value_name": "petalWidth"}, "85931696": {"index_name": "petalLength", "data_source": 80443440, "type": "ColormappedScatterPlot", "color_name": "species", "value_name": "petalLength"}, "85050384": {"index_name": "sepalWidth", "data_source": 80443440, "type": "ColormappedScatterPlot", "color_name": "species", "value_name": "petalLength"}, "87276336": {"width": 65.0, "plots": {"hello": ["87837200"]}, "type": "Plot", "height": 35.0}, "84276144": {"width": 65.0, "plots": {"hello": ["84374256"]}, "type": "Plot", "height": 35.0}, "87837200": {"index_name": "petalWidth", "data_source": 80443440, "type": "ColormappedScatterPlot", "color_name": "species", "value_name": "petalWidth"}, "87275952": {"index_name": "petalWidth", "data_source": 80443440, "type": "ColormappedScatterPlot", "color_name": "species", "value_name": "petalLength"}, "85932080": {"width": 65.0, "plots": {"hello": ["86493040"]}, "type": "Plot", "height": 35.0}, "82878832": {"width": 420, "shape": [4, 4], "type": "GridPlotContainer", "component_grid": [["82878928", "82919792", "83616304", "83722992"], ["84276144", "84374640", "84489424", "85050768"], ["85165648", "85718800", "85829584", "85932080"], ["86493424", "86608304", "87173744", "87276336"]], "height": 420}, "85829200": {"index_name": "petalLength", "data_source": 80443440, "type": "ColormappedScatterPlot", "color_name": "species", "value_name": "sepalWidth"}, "84489424": {"width": 65.0, "plots": {"hello": ["85050384"]}, "type": "Plot", "height": 35.0}, "82878928": {"width": 65.0, "plots": {"hello": ["82829136"]}, "type": "Plot", "height": 35.0}, "83615920": {"index_name": "sepalLength", "data_source": 80443440, "type": "ColormappedScatterPlot", "color_name": "species", "value_name": "sepalWidth"}, "86607920": {"index_name": "petalWidth", "data_source": 80443440, "type": "ColormappedScatterPlot", "color_name": "species", "value_name": "sepalLength"}, "86493424": {"width": 65.0, "plots": {"hello": ["86607920"]}, "type": "Plot", "height": 35.0}, "85050768": {"width": 65.0, "plots": {"hello": ["85165264"]}, "type": "Plot", "height": 35.0}, "86608304": {"width": 65.0, "plots": {"hello": ["87173360"]}, "type": "Plot", "height": 35.0}};
+window.main_id = "82878832";
+Backbone.sync = function(){};
+_.uniqueId = function (prefix) {
+    //from ipython project
+    // http://www.ietf.org/rfc/rfc4122.txt
+    var s = [];
+    var hexDigits = "0123456789ABCDEF";
+    for (var i = 0; i < 32; i++) {
+        s[i] = hexDigits.substr(Math.floor(Math.random() * 0x10), 1);
+    }
+    s[12] = "4";  // bits 12-15 of the time_hi_and_version field to 0010
+    s[16] = hexDigits.substr((s[16] & 0x3) | 0x8, 1);  // bits 6-7 of the clock_seq_hi_and_reserved to 01
+    var uuid = s.join("");
+    return prefix + "-" + uuid;
+};
+
+window.default_render_namespace = {}
+chaco = {}
+chaco.from_data = function(render_namespace, all_objs, obj_id, el){
+    var obj = all_objs[obj_id];
+    var obj_type = obj['type']
+    var model;
+    var collection;
+    var view;
+    if (obj_type ==='GridPlotContainer'){
+	if (!render_namespace['GridPlotContainers']){
+	    render_namespace['GridPlotContainers'] = new chaco.GridPlotContainers();
+	}
+	collection = render_namespace['GridPlotContainers'];
+	model = collection.create(obj);
+	args = {'collection' : collection,
+		'model' : model};
+	if(el){args['el'] = el}
+	view = new chaco.GridPlotContainerView(args);
+
+    }else if (obj_type == 'Plot'){
+	obj = _.clone(obj);
+	var sub_plot_id = obj.__.values(obj['plots'])[0];
+	var sub_obj = _.clone(all_objs[sub_plot_id]);
+	if (sub_obj['type'] === 'ColormappedScatterPlot'){
+	    _.extend(obj, sub_obj);
+	    if (!render_namespace['ColormappedScatterPlots']){
+		render_namespace['ColormappedScatterPlots'] = new chaco.ColormappedScatterPlots();
+	    }
+	    collection = render_namespace['ColormappedScatterPlots'];
+	    model = collection.create(obj);
+	    args = {'collection' : collection,
+		    'model' : model};
+	    if(el){args['el'] = el}
+	    view = new chaco.ColormappedScatterPlotview(args);
+	}
+    }
+    return {'collection' : collection,
+	    'model' : model,
+	    'view' : view}
+}
+//grid plot container model collection view
+chaco.GridPlotContainer = Backbone.Model.extend({
+    initialize : function(attributes, options){
+	if (!attributes['id']){
+	    this.set({'id' : _.uniqueId('view')}, 
+		     {silent : true})
+	}
+    },
+    defaults : {
+	'shape' : [],
+	'component_grid' : [[]],
+	'height' :  0,
+	'width' :  0,
+    }
+});
+
+chaco.GridPlotContainers = Backbone.Collection.extend({
+    model : chaco.GridPlotContainerModel,
+    url : "/",
+});
+
+chaco.GridPlotContainerView = Backbone.View.extend({
+    initialize : function(options){
+	if (!options['id']){
+	    this.id  = _.uniqueId('view');
+	}
+    },
+    render : function(){
+	this.$el.height(this.model.height);
+	this.$el.width(this.model.height);
+	_.each(this.model.component_grid, function(row){
+	    _.each(row, function(objid){
+		results = chaco.from_data(window.default_render_namespace,
+					  window.export_data, 
+					  objid);
+		this.$el.append(results['view'].$el);
+	    });
+	    this.$el.append($("<br/>"));
+	});
+    }
+});
+
+//color mapped scatter plot model, collection, view
+chaco.ColormappedScatterPlot = Backbone.Model.extend({
+    initialize : function(attributes, options){
+	if (!attributes['id']){
+	    this.set({'id' : _.uniqueId('view')}, 
+		     {silent : true})
+	}
+    },
+    defaults : {
+	'height' :  0,
+	'width' :  0,
+	'color_name' : '',
+	'index_name' : '',
+	'value_name' : '',
+	'data_source' : null
+    }
+});
+chaco.ColormappedScatterPlots = Backbone.Collection.extend({
+    model : chaco.ColormappedScatterPlot,
+    url : "/",
+});
+chaco.ColormappedScatterPlotview = Backbone.View.extend({
+    initialize : function(options){
+	if (!options['id']){
+	    this.id  = _.uniqueId('view');
+	}
+    },
+});
+
+$(function(){
+    results = chaco.from_data(window.default_render_namespace,
+			      window.export_data, 
+			      window.main_id, 
+			      $('#chart'));
+});
